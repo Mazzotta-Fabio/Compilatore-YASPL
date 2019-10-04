96cb22d113e2a36cf43b04a65e25ef5064e23707
Mazzotta-Fabio <f.mazzotta3@studenti.unisa.it>
2019-10-04 19:44:51 +0200

    effettuato testing sul progetto

--- sourceC.c
+++ sourceC.c
@@ -1,127 +1,29 @@
 #include<stdio.h> 
 #include<stdlib.h>
 
-int fine;
-int dato,op,dato1,dato2;
-int result1;
-double result2;
-double d1,d2;
-int y;
-int menu(int op){
-      int tipoOP;
-	printf("Seleziona l'operazione da eseguire: 1=addizione,2=sottrazione,3=moltiplicazione,4=divisione\n");
-	scanf("%d",&op);
-
-	tipoOP=op;
-	printf("Vuoi usare interi(0) o double(1)?\n");
-	scanf("%d",&dato);
-
-	if(dato==1)	{
-	printf("Inserisci il primo numero double\n");
-	scanf("%lf",&d1);
-
-	printf("Inserisci il secondo numero double\n");
-	scanf("%lf",&d2);
-
-	}
-
-	else	{
-	printf("Inserisci il primo numero intero\n");
-	scanf("%d",&dato1);
-
-	printf("Inserisci il secondo numero intero\n");
-	scanf("%d",&dato2);
-
-	}
-
-
-    return tipoOP;
-}
-
-int calcolaInt(int symbol,int tipo){
-      int resultInt;
-	if(tipo==0)	{
-	if(symbol==1)	{
-	resultInt=dato1+dato2;
-	}
-
-
-	if(symbol==2)	{
-	resultInt=dato1-dato2;
-	}
-
-
-	if(symbol==3)	{
-	resultInt=dato1*dato2;
-	}
-
-
-	if(symbol==4)	{
-	resultInt=dato1/dato2;
-	}
-
-
-	printf("Il risultato è %d\n",resultInt);
-	}
-
-	else	{
-	printf("ERRORE\n");
-	}
-
-
-    return resultInt;
-}
-
-double calcola(int symbol,int tipo){
-      double resultDouble;
-	if(tipo==1)	{
-	if(symbol==1)	{
-	resultDouble=d1+d2;
-	}
-
-
-	if(symbol==2)	{
-	resultDouble=d1-d2;
-	}
-
-
-	if(symbol==3)	{
-	resultDouble=d1*d2;
-	}
-
-
-	if(symbol==4)	{
-	resultDouble=d1/d2;
-	}
-
-
-	printf("Il risultato double è %lf\n",resultDouble);
-	}
-
-	else	{
-	printf("ERRORE\n");
-	}
-
-
-    return resultDouble;
-}
-
+double raggio,area;
+int i,n;
 int main(void){
-	fine=0;
-	while(fine==0)	{
-      op=menu(y);
-	if(dato==0)	{
-      result1=calcolaInt(op,dato);
+	printf("Quanti cerchi?\n");
+	scanf("%d",&n);
+
+	i=1;
+	while(n>0)	{
+	printf("cerchio n. %d   raggio: \n",i);
+	scanf("%lf",&raggio);
+
+	if(raggio<0.0)	{
+	printf("valore del raggio non valido \n");
 	}
 
 	else	{
-      result2=calcola(op,dato);
+	area=3.14*(raggio*raggio);
+	printf("area: %lf \n",area);
 	}
 
 
-	printf("Vuoi continuare? (digita 0 per continuare o 1 per uscire)\n");
-	scanf("%d",&fine);
-
+	n=n-1;
+	i=i+1;
 	}
 
 
